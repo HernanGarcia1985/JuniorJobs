4fd941f0690f167f76349e8ffbc84acd9fb482d7
HernanGarcia1985 <hernang1985@gmail.com>
2022-05-05 11:57:33 -0300

    Me modificó scheduleController

--- src/controllers/scheduleController.js
+++ src/controllers/scheduleController.js
@@ -2,6 +2,24 @@ const db = require('../database/models');
 
 const scheduleController = {
 
+    show: (req, res) => {
+        //res.json("Metodo para mostrar Schedule por el ID");
+        db.Schedule.findByPk(req.params.id)
+            .then((schedule) => {
+                if(schedule) {
+                    console.log(schedule);
+                    return res.status(200).json(schedule.dataValues);
+                }
+                else {
+                    console.log('No se encontró el schedule en nuestra base de datos');
+                    return res.status(404).json({message: 'No se encontró el schedule en nuestra base de datos'});
+                }
+            })
+            .catch(function(error){
+                console.log(`Se ha producido el siguiente error: `, error);
+            })
+    },
+
     index: (req, res) => {
         //res.json("Metodo visualización de todos los Schedules");
         db.Schedule.findAll()
@@ -18,23 +36,7 @@ const scheduleController = {
         .catch(function(error){
             console.log(`Se ha producido el siguiente error: `, error);
         })
-    }, 
-
-
-    show: (req, res) => {
-        const id = req.params.id;
-        db.Schedule.findOne({where:{id: id}})
-        .then((schedule) => {
-            if(schedule){
-                console.log(schedule)
-                res.json(schedule)
-            } else {
-                res.json({message: "no existe el schedule buscado"})
-            }
-        })
-    },
-
-    
+    }    
 }
 
 module.exports = scheduleController;
