fb185a29fd1dae68dffd1045138fc7cc61fcebb5
HernanGarcia1985 <hernang1985@gmail.com>
2022-05-05 11:51:31 -0300

    Me modificó remoteController

--- src/controllers/remoteController.js
+++ src/controllers/remoteController.js
@@ -2,6 +2,24 @@ const db = require('../database/models');
 
 const remoteController = {
 
+    show: (req, res) => {
+        //res.json("Metodo para mostrar Remote por el ID");
+        db.Remote.findByPk(req.params.id)
+            .then((remote) => {
+                if(remote) {
+                    console.log(remote);
+                    return res.status(200).json(remote.dataValues);
+                }
+                else {
+                    console.log('No se encontró el remote en nuestra base de datos');
+                    return res.status(404).json({message: 'No se encontró el remote en nuestra base de datos'});
+                }
+            })
+            .catch(function(error){
+                console.log(`Se ha producido el siguiente error: `, error);
+            })
+    },
+
     index: (req, res) => {
         //res.json("Metodo visualización de todos los Remotes");
         db.Remote.findAll()
@@ -18,20 +36,7 @@ const remoteController = {
         .catch(function(error){
             console.log(`Se ha producido el siguiente error: `, error);
         })
-    },
-
-    show: (req, res) => {
-        const id = req.params.id;
-        db.Remote.findOne({where:{id: id}})
-        .then((remote) => {
-            if(remote){
-                console.log(remote)
-                res.json(remote)
-            } else {
-                res.json({message: "no existe el remote buscado"})
-            }
-        })
-    },
+    }
 }
 
 module.exports = remoteController;
